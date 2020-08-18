0da2bc40a11076e45c1336fa0f4f2ba5ad905b24
kiki7000 <51841836+kiki7000@users.noreply.github.com>
2020-08-18 17:04:40 +0900

    Create run.py

---
+++ run.py
@@ -0,0 +1,7 @@
+from interpreter import Interpreter
+
+def run_string(string: str): Interpreter().run(string)
+def run_file(name: str): run_string(open(str(name), 'r', encoding = 'utf-8').read())
+
+run_file('test.sansscript')
+run_string('샌즈 (키키')
