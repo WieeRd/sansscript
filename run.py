a893ad5845be306855015682a51b6bca48927e4e
kiki7000 <51841836+kiki7000@users.noreply.github.com>
2020-08-20 17:12:36 +0900

    zlzl

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ def run_string(string: str): Interpreter().run(string)
 def run_file(name: str): run_string(open(str(name), 'r', encoding = 'utf-8').read())
 
 run_file('test.sansscript')
-run_string('샌즈 (키키')
+run_string('샌즈 (키키)')
